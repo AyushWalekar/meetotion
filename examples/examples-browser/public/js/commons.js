8617cdd06af34ca58b9e2fe83e7b6e3352135a77
Ayush Walekar <walekarayush@gmail.com>
2022-07-15 14:42:35 +0530

    removed un-used components from UI

--- examples/examples-browser/public/js/commons.js
+++ examples/examples-browser/public/js/commons.js
@@ -23,68 +23,68 @@ async function requestExternalImage(imageUrl) {
 }
 
 function renderNavBar(navbarId, exampleUri) {
-  const examples = [
-    {
-      uri: 'webcam_face_expression_recognition',
-      name: 'Face Expression Recognition'
-    }
-  ]
+  // const examples = [
+  //   {
+  //     uri: 'webcam_face_expression_recognition',
+  //     name: 'Face Expression Recognition'
+  //   }
+  // ]
 
-  const navbar = $(navbarId).get(0)
-  const pageContainer = $('.page-container').get(0)
+  // const navbar = $(navbarId).get(0)
+  // const pageContainer = $('.page-container').get(0)
 
-  const header = document.createElement('h3')
-  header.innerHTML = examples.find(ex => ex.uri === exampleUri).name
-  pageContainer.insertBefore(header, pageContainer.children[0])
+  // const header = document.createElement('h3')
+  // header.innerHTML = examples.find(ex => ex.uri === exampleUri).name
+  // pageContainer.insertBefore(header, pageContainer.children[0])
 
-  const menuContent = document.createElement('ul')
-  menuContent.id = 'slide-out'
-  menuContent.classList.add('side-nav', 'fixed')
-  navbar.appendChild(menuContent)
+  // const menuContent = document.createElement('ul')
+  // menuContent.id = 'slide-out'
+  // menuContent.classList.add('side-nav', 'fixed')
+  // navbar.appendChild(menuContent)
 
-  const menuButton = document.createElement('a')
-  menuButton.href = '#'
-  menuButton.classList.add('button-collapse', 'show-on-large')
-  menuButton.setAttribute('data-activates', 'slide-out')
-  const menuButtonIcon = document.createElement('img')
-  menuButtonIcon.src = 'menu_icon.png'
-  menuButton.appendChild(menuButtonIcon)
-  navbar.appendChild(menuButton)
+  // const menuButton = document.createElement('a')
+  // menuButton.href = '#'
+  // menuButton.classList.add('button-collapse', 'show-on-large')
+  // menuButton.setAttribute('data-activates', 'slide-out')
+  // const menuButtonIcon = document.createElement('img')
+  // menuButtonIcon.src = 'menu_icon.png'
+  // menuButton.appendChild(menuButtonIcon)
+  // navbar.appendChild(menuButton)
 
-  const li = document.createElement('li')
-  const githubLink = document.createElement('a')
-  githubLink.classList.add('waves-effect', 'waves-light', 'side-by-side')
-  githubLink.id = 'github-link'
-  githubLink.href = 'https://example.com'
-  const h5 = document.createElement('h5')
-  h5.innerHTML = 'Meetotion'
-  githubLink.appendChild(h5)
-  const githubLinkIcon = document.createElement('img')
-  githubLinkIcon.src = 'github_link_icon.png'
-  githubLink.appendChild(githubLinkIcon)
-  li.appendChild(githubLink)
-  menuContent.appendChild(li)
+  // const li = document.createElement('li')
+  // const githubLink = document.createElement('a')
+  // githubLink.classList.add('waves-effect', 'waves-light', 'side-by-side')
+  // githubLink.id = 'github-link'
+  // githubLink.href = 'https://example.com'
+  // const h5 = document.createElement('h5')
+  // h5.innerHTML = 'Meetotion'
+  // githubLink.appendChild(h5)
+  // const githubLinkIcon = document.createElement('img')
+  // githubLinkIcon.src = 'github_link_icon.png'
+  // githubLink.appendChild(githubLinkIcon)
+  // li.appendChild(githubLink)
+  // menuContent.appendChild(li)
 
-  examples
-    .forEach(ex => {
-      const li = document.createElement('li')
-      if (ex.uri === exampleUri) {
-        li.style.background = '#b0b0b0'
-      }
-      const a = document.createElement('a')
-      a.classList.add('waves-effect', 'waves-light', 'pad-sides-sm')
-      a.href = ex.uri
-      const span = document.createElement('span')
-      span.innerHTML = ex.name
-      span.style.whiteSpace = 'nowrap'
-      a.appendChild(span)
-      li.appendChild(a)
-      menuContent.appendChild(li)
-    })
+  // examples
+  //   .forEach(ex => {
+  //     const li = document.createElement('li')
+  //     if (ex.uri === exampleUri) {
+  //       li.style.background = '#b0b0b0'
+  //     }
+  //     const a = document.createElement('a')
+  //     a.classList.add('waves-effect', 'waves-light', 'pad-sides-sm')
+  //     a.href = ex.uri
+  //     const span = document.createElement('span')
+  //     span.innerHTML = ex.name
+  //     span.style.whiteSpace = 'nowrap'
+  //     a.appendChild(span)
+  //     li.appendChild(a)
+  //     menuContent.appendChild(li)
+  //   })
 
-  $('.button-collapse').sideNav({
-    menuWidth: 260
-  })
+  // $('.button-collapse').sideNav({
+  //   menuWidth: 260
+  // })
 }
 
 function renderSelectList(selectListId, onChange, initialValue, renderChildren) {
