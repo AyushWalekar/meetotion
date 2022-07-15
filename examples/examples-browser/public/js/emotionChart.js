128aecf2728389a4d2b10db8e2ca84bd0e9911c3
Ayush Walekar <walekarayush@gmail.com>
2022-07-15 18:41:09 +0530

    added checkbox for chart generation

--- examples/examples-browser/public/js/emotionChart.js
+++ examples/examples-browser/public/js/emotionChart.js
@@ -1,11 +1,10 @@
 function createEmotionLineChart(expressionsRecorded) {
-    //TOOD: add param for types of chart to display
-
     const angryValues = [];
     const happyValues = [];
     const sadValues = [];
-    // const angryValues = [];
-    // const angryValues = [];
+    const surprisedValues = [];
+    const neutralValues = [];
+    const fearfulValues = [];
 
     const startDate = new Date().toDateString();
 
@@ -14,7 +13,9 @@ function createEmotionLineChart(expressionsRecorded) {
         angryValues.push(expressionsRecorded[timeStampArr[i]].angry);
         happyValues.push(expressionsRecorded[timeStampArr[i]].happy);
         sadValues.push(expressionsRecorded[timeStampArr[i]].sad);
-        //TODO: others
+        surprisedValues.push(expressionsRecorded[timeStampArr[i]].surprised);
+        neutralValues.push(expressionsRecorded[timeStampArr[i]].neutral);
+        fearfulValues.push(expressionsRecorded[timeStampArr[i]].fearful);
     }
 
     timeStampArr = timeStampArr.map(val => {
@@ -22,32 +23,76 @@ function createEmotionLineChart(expressionsRecorded) {
         return d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
     })
 
+    const dataset = [];
+    if (document.getElementById('happy').checked) {
+        dataset.push({
+            data: happyValues,
+            label: "happy",
+            borderColor: "#F8DE7E",
+            fill: false
+        })
+    }
+    if (document.getElementById('sad').checked) {
+        dataset.push({
+            data: sadValues,
+            label: "sad",
+            borderColor: "#3e95cd",
+            fill: false
+        })
+    }
+    if (document.getElementById('angry').checked) {
+        dataset.push({
+            data: angryValues,
+            label: "angry",
+            borderColor: "#C45850",
+            fill: false
+        })
+    }
+    if (document.getElementById('surprised').checked) {
+        dataset.push({
+            data: surprisedValues,
+            label: "surprised",
+            borderColor: "#3e95cd",
+            fill: false
+        })
+    }
+    if (document.getElementById('neutral').checked) {
+        dataset.push({
+            data: neutralValues,
+            label: "neutral",
+            borderColor: "#E8C3B9",
+            fill: false
+        })
+    }
+    if (document.getElementById('fearful').checked) {
+        dataset.push({
+            data: fearfulValues,
+            label: "fearful",
+            borderColor: "##8E5EA2",
+            fill: false
+        })
+    }
+
     new Chart(document.getElementById("line-chart"), {
         type: 'line',
         data: {
             labels: timeStampArr,
-            datasets: [{
-                data: happyValues,
-                label: "happy",
-                borderColor: "#3e95cd",
-                fill: false
-            }, {
-                data: sadValues,
-                label: "sad",
-                borderColor: "#8e5ea2",
-                fill: false
-            }, {
-                data: angryValues,
-                label: "angry",
-                borderColor: "#3cba9f",
-                fill: false
-            },
-            ]
+            datasets: dataset
         },
         options: {
             title: {
                 display: true,
                 text: 'Meetotion Report: ' + startDate
+            },
+            scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        suggestedMin: 0,
+                        beginAtZero: true,
+                        max: 1
+                    }
+                }]
             }
         }
     });
